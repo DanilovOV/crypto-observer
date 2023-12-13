d87bed0a1f178330f28f09fac6774cc9c13bf195
Олег Данилов <ovdanoleg8@gmail.com>
2023-12-13 21:01:13 +0300

    upd: tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-   darkMode: false,
-   theme: {
-     extend: {},
-   },
-   variants: {
-     extend: {},
-   },
-   plugins: [
-    require('@tailwindcss/forms'),
-   ],
+	purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+	media: false,
+	theme: {
+		extend: {},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [require('@tailwindcss/forms')],
 }
-
