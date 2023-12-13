f9f31739fc42b40272f78d607d94875dba704bbc
Олег Данилов <ovdanoleg8@gmail.com>
2023-12-14 02:49:10 +0300

    add: header footer

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <template>
+	<Header />
 	<main-page />
 </template>
 
 <script>
-import MainPage from './views/Mainpage.vue'
+import MainPage from './views/MainPage.vue'
+import Header from './components/layout/Header.vue'
+
 export default {
 	components: {
 		MainPage,
+		Header,
 	},
 }
 </script>
---
+++ src/components/layout/Header.vue
@@ -0,0 +1,83 @@
+<template>
+	<header class="site-header">
+		<div class="logo">
+			<img
+				src="/path/to/your/logo.png"
+				alt="Логотип"
+			/>
+		</div>
+
+		<nav class="main-menu">
+			<ul>
+				<li><router-link to="/">Главная</router-link></li>
+				<li><router-link to="/about">О нас</router-link></li>
+				<li><router-link to="/services">Услуги</router-link></li>
+				<li><router-link to="/services">Услуги</router-link></li>
+			</ul>
+		</nav>
+
+		<div class="search-bar">
+			<input
+				type="text"
+				placeholder="Поиск"
+				v-model="searchQuery"
+			/>
+			<button @click="search">Искать</button>
+		</div>
+	</header>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			searchQuery: '',
+		}
+	},
+	methods: {
+		search() {
+			console.log('Выполняется поиск:', this.searchQuery)
+		},
+	},
+}
+</script>
+
+<style scoped>
+.site-header {
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	padding: 20px;
+	background-color: #f0f0f0;
+}
+
+.logo img {
+	max-height: 50px; /* Регулируйте высоту логотипа по необходимости */
+}
+
+.main-menu ul {
+	list-style-type: none;
+	display: flex;
+	gap: 20px;
+}
+
+.main-menu li {
+	font-size: 16px;
+}
+
+.search-bar {
+	display: flex;
+	gap: 10px;
+}
+
+.search-bar input {
+	padding: 8px;
+	font-size: 14px;
+}
+
+.search-bar button {
+	padding: 8px;
+	font-size: 14px;
+	cursor: pointer;
+}
+</style>
