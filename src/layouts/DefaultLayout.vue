26001d63ef9e96551b8e73703deeabd8f4dea2f2
Олег Данилов <ovdanoleg8@gmail.com>
2024-01-20 00:54:34 +0300

    ref: delete router import from layout

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -9,14 +9,12 @@
 </template>
 
 <script>
-import { RouterView } from 'vue-router'
 import WrapperComponent from '@/components/layout/WrapperComponent.vue'
 import HeaderLayout from '@/components/layout/HeaderLayout.vue'
 import FooterLayout from '@/components/layout/FooterLayout.vue'
 import ContentLayout from '@/components/layout/ContentLayout.vue'
 export default {
 	components: {
-		RouterView,
 		WrapperComponent,
 		HeaderLayout,
 		FooterLayout,
