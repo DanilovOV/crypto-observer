4380bea40c6ab008b3f2c78480d2a47fe56663bf
Олег Данилов <oleg-danilov8@ya.ru>
2024-02-25 02:12:41 +0300

    fix configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,5 @@
-/** @type {import('tailwindcss').Config} */
 export default {
 	purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
 	media: false,
-	theme: {
-		extend: {},
-	},
-	variants: {
-		extend: {},
-	},
 	plugins: [require('@tailwindcss/forms')],
 }
