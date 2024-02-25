37fe1c090a135e8dd0c61e33d7231e92441d6542
Олег Данилов <oleg-danilov8@ya.ru>
2024-02-26 02:46:26 +0300

    fix header styles

--- src/components/layout/TheHeader.vue
+++ src/components/layout/TheHeader.vue
@@ -1,51 +1,12 @@
 <template>
-	<header class="site-header">
-		<div class="logo flex items-center">
+	<header class="flex justify-between items-center p-5 bg-gray-200">
+		<div class="flex items-center">
 			<img
 				src="/icons/favicon-48x48.png"
 				alt="Crypto observer"
+				class="max-h-[50px] mr-2"
 			/>
 			<div>Crypto-Observer</div>
 		</div>
 	</header>
 </template>
-
-<style scoped>
-.site-header {
-	display: flex;
-	justify-content: space-between;
-	align-items: center;
-	padding: 20px;
-	background-color: #f0f0f0;
-}
-
-.logo img {
-	max-height: 50px;
-}
-
-.main-menu ul {
-	list-style-type: none;
-	display: flex;
-	gap: 20px;
-}
-
-.main-menu li {
-	font-size: 16px;
-}
-
-.search-bar {
-	display: flex;
-	gap: 10px;
-}
-
-.search-bar input {
-	padding: 8px;
-	font-size: 14px;
-}
-
-.search-bar button {
-	padding: 8px;
-	font-size: 14px;
-	cursor: pointer;
-}
-</style>
